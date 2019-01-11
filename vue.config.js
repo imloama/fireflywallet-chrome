39d54fbcf58cd762fdd5947a9b6ccaea3348be19
imloama <imloama@gmail.com>
2019-01-11 16:31:48 +0800

    feat: 添加webpack配置文件
    添加webpack配置文件，由于vue-cli3未配置成功

--- vue.config.js
+++ vue.config.js
@@ -7,40 +7,69 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { version } = require('./package.json');
 
 module.exports = {
+    publicPath:"",
+    pages:{
+        app:{
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            chunks:['app']
+        },
+        // background:{
+        //     entry: 'src/background.js',
+        //     filename: 'background.html',
+        //     chunks:false
+        // },
+        // chromereload:{
+        //     entry: 'src/chromereload.js',
+        //     template: 'public/index.html',
+        //     filename: 'chromereload.html',
+        //     chunks:false
+        // },
+        // ffw:{
+        //     entry: 'src/ffw/ffw.js',
+        //     template: 'public/index.html',
+        //     filename: 'ffw.html',
+        //     chunks:false
+        // }
+
+    },
+    productionSourceMap: false,
     chainWebpack: (config)=>{
         config.resolve.alias
             .set('@', resolve('src'))
             .set('components',resolve('src/components'));
-        config.entry('background')
-            .add(resolve('src')+'/background.js')
-            .end()
-            .entry('chromereload')
-            .add(resolve('src')+'/chromereload.js')
-            .end()
-            // .entry('manifest')
-            // .add('src/manifest.json')
-            // .end()
-            // ;
-            .output
-                //.path('dist')
-                .filename('[name].js');
+        
+        // config
+        //     .entry('background')
+        //     .add(resolve('src')+'/background.js')
+        //     .end()
+        //     .entry('chromereload')
+        //     .add(resolve('src')+'/chromereload.js')
+        //     .end()
+        //     .entry('ffw')
+        //     .add(resolve('src')+'/ffw/ffw.js')
+        //     .end()
+        //     .output
+        //         .filename('[name].js');
         
         
         config.plugin('CopyWebpackPlugin')
                 .use(new CopyWebpackPlugin([
-                    {
-                      from: 'src/manifest.json',
-                      to: 'manifest.json',
-                      transform: (content) => {
-                        const jsonContent = JSON.parse(content);
-                        jsonContent.version = version;
-                        if (config.mode === 'development') {
-                          jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
-                        }
-                        return JSON.stringify(jsonContent, null, 2);
-                      },
-                    },
-                    {from:'src/_locales', to: '_locales'}
+                    // {
+                    //   from: 'src/manifest.json',
+                    //   to: 'manifest.json',
+                    //   transform: (content) => {
+                    //     const jsonContent = JSON.parse(content);
+                    //     jsonContent.version = version;
+                    //     if (config.mode === 'development') {
+                    //       jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
+                    //     }
+                    //     return JSON.stringify(jsonContent, null, 2);
+                    //   },
+                    // },
+                    // {from:'src/_locales', to: '_locales'},
+                    {from:'src/icons', to: 'icons'}
                   ])
               )
     }
